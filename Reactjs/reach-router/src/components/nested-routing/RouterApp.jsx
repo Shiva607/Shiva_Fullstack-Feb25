7d237759dbf5b74c736a779aaff5cc0535e08179
Shiva Sankar J <jshivasankar607@gmail.com>
2025-05-03 18:41:26 +0530

    Refactor RouterApp and UserProfile components for consistency and readability; adjust import styles and formatting

--- Reactjs/reach-router/src/components/nested-routing/RouterApp.jsx
+++ Reactjs/reach-router/src/components/nested-routing/RouterApp.jsx
@@ -1,27 +1,27 @@
-import { Link, Router } from "@reach/router";
-import React from "react";
-import Home from "./Home";
-import UserProfile from "./UserProfile";
-import UserDetails from "./UserDetails";
-import UserPosts from "./UserPosts";
+import { Link, Router } from '@reach/router';
+import React from 'react';
+import Home from './Home';
+import UserProfile from './UserProfile';
+import UserDetails from './UserDetails';
+import UserPosts from './UserPosts';
 
 export default function RouterApp() {
-  return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link> |&nbsp;
-        <Link to="/user/1">User 1 Profile</Link> |&nbsp;
-        <Link to="/user/2">User 2 Profile</Link> |&nbsp;
-        <Link to="/user/3">User 3 Profile</Link>
-      </nav>
+	return (
+		<div>
+			<nav>
+				<Link to="/">Home</Link> |&nbsp;
+				<Link to="/user/1">User 1 Profile</Link> |&nbsp;
+				<Link to="/user/2">User 2 Profile</Link> |&nbsp;
+				<Link to="/user/3">User 3 Profile</Link>
+			</nav>
 
-      <Router>
-        <Home path="/" />
-        <UserProfile path="/user/:id">
-          <UserDetails path="details" />
-          <UserPosts path="posts" />
-        </UserProfile>
-      </Router>
-    </div>
-  );
+			<Router>
+				<Home path="/" />
+				<UserProfile path="/user/:id">
+					<UserDetails path="details" userName="Shiva" />
+					<UserPosts path="posts" />
+				</UserProfile>
+			</Router>
+		</div>
+	);
 }
--- Reactjs/reach-router/src/components/nested-routing/UserProfile.jsx
+++ Reactjs/reach-router/src/components/nested-routing/UserProfile.jsx
@@ -1,27 +1,34 @@
-import { Link } from "@reach/router";
-import React from "react";
+import { Link } from '@reach/router';
+import React from 'react';
 
 export default function UserProfile({ id, children }) {
-  const userData = {
-    1: { name: "Rama", age: 28 },
-    2: { name: "Krishna", age: 34 },
-  };
+	const userData = {
+		1: { name: 'Rama', age: 28 },
+		2: { name: 'Krishna', age: 34 },
+	};
 
-  const user = userData[id] || { name: "Unknown", age: "N/A" };
-  const userExists = !!userData[id];
+	const user = userData[id] || { name: 'Unknown', age: 'N/A' };
+	const userExists = !!userData[id];
 
-  return (
-    <div>
-      <h2>User Profile: {user.name}</h2>
-      <p>Age: {user.age}</p>
-      <hr />
-      {userExists && (
-        <div>
-          <Link to="details">Details</Link>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-          <Link to="posts">Posts</Link>
-        </div>
-      )}
-      {children}
-    </div>
-  );
+	//! Get the child attribute value
+	console.log(children);
+	console.log(children.props.children[0].props);
+	const { path, userName } = children.props.children[0].props;
+	console.log(path);
+	console.log(userName);
+
+	return (
+		<div>
+			<h2>User Profile: {user.name}</h2>
+			<p>Age: {user.age}</p>
+			<hr />
+			{userExists && (
+				<div>
+					<Link to="details">Details</Link>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+					<Link to="posts">Posts</Link>
+				</div>
+			)}
+			{children}
+		</div>
+	);
 }
