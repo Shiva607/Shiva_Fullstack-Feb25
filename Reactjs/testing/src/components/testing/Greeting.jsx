9d077ce89aa98051b701e650ded84b865b5c2f19
Shiva Sankar J <jshivasankar607@gmail.com>
2025-05-10 22:29:42 +0530

    Update .gitignore to exclude .vscode directory; add VSCode configuration files for extensions, keybindings, and settings

--- Reactjs/testing/src/components/testing/Greeting.jsx
+++ Reactjs/testing/src/components/testing/Greeting.jsx
@@ -9,6 +9,15 @@ export default function Greeting() {
 				// The data-testid attribute is used in testing to uniquely identify and reliably select elements in the DOM without relying on visible text or styles.
 			}
 			<h1 data-testid="greeting-text">{greeting}</h1>
+			{
+				{
+					//! Arial attributes
+					/*
+					 * The aria-label attribute is used to provide an accessible name for the button, which is important for screen readers and other assistive technologies.
+					 * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA
+					 */
+				}
+			}
 			<button aria-label="Change Greeting" onClick={() => setGreeting('Welcome, Guest!')}>
 				Change Greeting
 			</button>
